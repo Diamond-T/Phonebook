a077b16bb6d58f4b00fa18b748eb57873f6daccb
Diamond Toliver <diamondtoliver@Diamonds-MacBook-Pro.local>
2021-08-05 03:10:12 -0400

    errors

---
+++ routes.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template
+from app.templates import RegisterForm
+
+@app.route('/')
+def index():
+    name = 'Diamond'
+    title = 'Coding Temple Intro to Flask'
+    return render_template('home.html', name=name, title=title)
+
+@app.route('/home', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        print ('THE FORM IS VALID')
+        username = form.username.data
+        phone_number= form.phone_number.data
+        
+
